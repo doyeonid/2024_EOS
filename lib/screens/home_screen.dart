755b71c06b21f7b5ba13362eed7191dcf24b09e9
doyeonid <doyeon05@hanyang.ac.kr>
2024-11-11 13:16:29 +0900

    EOS

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -11,7 +11,8 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  List todoLists = [];
+  List<String> todoLists = [];
+
   @override
   void initState() {
     super.initState();
@@ -21,124 +22,132 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
   @override
-  void dispose() {
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Color(0xFFA4C639).withOpacity(0.1),
-        title: Text('EOS ToDoList'),
-        leading: Image.asset('assets/images/eos_logo.png'),
+        title: Row(
+          children: [
+            Image.asset(
+              'assets/images/eos_logo.png',
+              width: 30,
+              height: 30,
+            ),
+            SizedBox(width: 8),
+            Text('EOS ToDoList'),
+            Spacer(),
+            Stack(
+              children: [
+                Positioned(
+                  right: 0,
+                  child: Container(
+                    padding: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+                    color: Colors.redAccent,
+                    child: Text(
+                      "NEW",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 12,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
       body: Column(
         children: [
-          Container(
-            padding: EdgeInsets.all(25),
-            height: 200,
+          Padding(
+            padding: const EdgeInsets.all(20.0),
             child: Row(
               children: [
                 Container(
-                  width: 140,
-                  height: 140,
+                  width: 80,
+                  height: 80,
                   decoration: BoxDecoration(
-                    color: Colors.white,
-                    border: Border.all(width: 10, color: Colors.grey),
-                    borderRadius: BorderRadius.circular(100),
+                    color: Colors.grey[300],
+                    borderRadius: BorderRadius.circular(40),
                   ),
                   child: Center(
                     child: Image.asset('assets/images/eos_logo.png'),
                   ),
                 ),
-                SizedBox(
-                  width: 35,
+                SizedBox(width: 20),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      '최윤영(본인 이름)',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 5),
+                    Text(
+                      '4.0 이상 학점 받기(열심히 다짐!)',
+                      style: TextStyle(
+                        fontSize: 14,
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ],
                 ),
-                Expanded(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        '김도연',
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      SizedBox(
-                        height: 15,
-                      ),
-                      Text('hello!')
-                    ],
-                  ),
-                )
               ],
             ),
           ),
-          Stack(
-            children: [
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 25),
-                child: Container(
-                  width: MediaQuery.of(context).size.width,
-                  height: 500,
-                  decoration: BoxDecoration(
-                    color: Color(0xFFA4C639).withOpacity(0.1),
-                    borderRadius: BorderRadius.circular(20),
+          Container(
+            padding: EdgeInsets.all(10),
+            margin: EdgeInsets.symmetric(horizontal: 20),
+            decoration: BoxDecoration(
+              color: Color(0xFFA4C639).withOpacity(0.1),
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: Column(
+              children: [
+                Container(
+                  padding: EdgeInsets.symmetric(vertical: 10),
+                  child: Text(
+                    "To do list",
+                    style: TextStyle(
+                      color: Colors.black87,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
+                ),
+                Container(
+                  height: 300, // 리스트가 표시될 영역의 높이 조절
                   child: ListView.builder(
                     itemCount: todoLists.length,
                     itemBuilder: (BuildContext context, int index) {
                       return ToDoItem(
-                        title: todoLists[index], // title 전달
+                        title: todoLists[index],
                         onDelete: () {
                           setState(() {
                             todoLists.removeAt(index);
                           });
-                        }, // onDelete 전달
+                        },
                       );
                     },
                   ),
                 ),
-              ), // 여기 쉼표 추가
-              Positioned(
-                bottom: 30,
-                right: 50,
-                child: AddButton(
-                  onPressed: () {
-                    setState(() {
-                      todoLists.add("+++++++++");
-                    });
-                  },
-                ),
-              ),
-              Positioned(
-                top: 20,
-                left: (MediaQuery.of(context).size.width - 150) / 2,
-                child: Container(
-                  width: 150,
-                  height: 38,
-                  decoration: BoxDecoration(
-                    color: Color(0xFFA4C639).withOpacity(0.3),
-                    borderRadius: BorderRadius.circular(30),
-                  ),
-                  child: Center(
-                    child: Text(
-                      "To do list",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 23,
-                        fontWeight: FontWeight.w600,
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          )
+              ],
+            ),
+          ),
         ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            todoLists.add("새로운 항목");
+          });
+        },
+        backgroundColor: Color(0xFFA4C639),
+        child: Icon(Icons.add),
       ),
     );
   }
